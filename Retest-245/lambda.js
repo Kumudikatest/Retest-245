985c1e0fa0cd8af5cdc2e5ab32789d30e2174090
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-01-28 16:57:58 +0530

    change input type

--- Retest-245/lambda.js
+++ Retest-245/lambda.js
@@ -5,7 +5,18 @@ exports.handler = function (event, context, callback) {
     ddb.scan({
         TableName: 'test',
         ExpressionAttributeValues: {
-            ':Age': 14
+            ':Age': 14,
+            ':Name': 'NaN'
+        }
+    }).promise().then(function (data) {
+        //your logic goes here
+    }).catch(function (err) {
+        //handle error
+    });
+    ddb.scan({
+        TableName: 'test',
+        ExpressionAttributeValues: {
+            ':Subject': 'NaN'
         }
     }).promise().then(function (data) {
         //your logic goes here
